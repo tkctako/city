7bb2398981b422da8a5b4915882c8fe078f971a6
tako9555 <tako9555@gmail.com>
2022-10-14 09:23:47 +0800

    變更密碼畫面設計

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChangePasswordComponent },
+  { path: 'changepassword', component: ChangePasswordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
